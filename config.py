0b4a5bcd70f59cf6e3eaf7b0871f2e9ea5b57e34
xiaoxiaoya555 <514627615@qq.com>
2024-09-14 02:45:14 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+START_DATE= "2023-08-07"
+CITY= "lunan"
+BIRTHDAY="11-27"
+APP_ID= "wxb096096bc0eae021"
+APP_SECRET= "e8f70febe103a53d1061b1d1cec8b12f"
+USER_ID= "old0c6o_z0B6bJlmJ6sxXGNuY4Pw"
+TEMPLATE_ID= "nKV5UAn4JQ_cJWDvzU7q7lTgcTh4oqIpG_cMdWqAcd4"
+api_key =  "187c8ac2abfa4c7d95971b02e727d2e5"
+word_key="a9ed91c8b9c1c72ecdb64eea2312d698"
+banci=[
+    '中', '中', '中', '晚', '休', '休', '早', '早', '中', '晚', '晚', '休', '休', '早',
+    '早', '早', '早', '中', '休', '早', '早', '早', '中', '中', '中', '休', '早', '中',
+    '中', '晚'
+]
